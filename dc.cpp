7d0fbfc40ec4a1a4c19cb693653c1405fd9cc3e3
Luis Marval <Luismarval5a@hotmail.com>
2023-04-08 14:52:48 -0400

    Reproductor videojs

---
+++ dc.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<stdlib.h>
+using namespace std;
+
+int  main(){
+	int a;
+	cin>>a;
+	if(a%2==0){cout<<"par";}
+	else {cerr<<"el numero es inadecuado";}
+	system("PAUSE");
+}
